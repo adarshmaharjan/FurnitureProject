5a051e3ed6f9babdd433785641ba9d1c690623b5
adarshmaharjan <40669144+adarshmaharjan@users.noreply.github.com>
2021-01-08 08:19:26 +0545

    furniture project

--- js/main.js
+++ js/main.js
@@ -1,6 +1,21 @@
+/*overlay*/
 function showNav() {
     document.getElementById('myNav').style.display = 'block';
 }
 function closeNav() {
     document.getElementById('myNav').style.display = 'none';
+}
+/*sticky bar*/
+window.onscroll =function() {stickyFunction()};
+
+var navbar = document.getElementById('sticky-bar');
+
+var sticky = navbar.offsetTop;
+
+function stickyFunction(){
+    if (window.pageYOffset >= sticky) {
+        navbar.classList.add("sticky");
+    }else{
+        navbar.classList.remove('sticky');
+    }
 }
